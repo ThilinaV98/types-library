4bc7f6de541f0b1d7f86be9c2f524288126e081d
Thilina Upeksha <thilinavithana98@gmail.com>
2025-02-03 16:58:29 +0530

    Initial commit of Project files

---
+++ dist/types.d.ts
@@ -0,0 +1,23 @@
+export type TMarkAsBlocked = {
+    uniqueCode: string;
+    tenantId: string;
+    token: string;
+    reason: string;
+};
+export type TMarkAsResolved = {
+    uniqueCode: string;
+    tenantId: string;
+    token: string;
+};
+export type TMarkAsInvalid = {
+    uniqueCode: string;
+    tenantId: string;
+    token: string;
+    reason: string;
+};
+export type TMarkAsValid = {
+    uniqueCode: string;
+    tenantId: string;
+    token: string;
+};
+//# sourceMappingURL=types.d.ts.map
