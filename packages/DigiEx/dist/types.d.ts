6f5b2f6b27f2ffcb6141afa252067dcacf85fa40
Thilina Upeksha <thilinavithana98@gmail.com>
2025-02-11 09:27:20 +0530

    Removed dist from git tracking

--- packages/DigiEx/dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-export type DigiExTypes = {
-    id: string;
-    name: string;
-    mediaType: "image" | "video";
-    createdAt: string;
-};
-//# sourceMappingURL=types.d.ts.map
--- packages/Digieye/dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type DigiEyeTypes = {
-    id: string;
-    name: string;
-    templateId: string;
-    templateName: string;
-    templateLabel: string;
-};
-//# sourceMappingURL=types.d.ts.map
