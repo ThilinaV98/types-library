f83847e150760cbea4a15185e72009438c8d071c
Thilina Upeksha <thilinavithana98@gmail.com>
2025-02-28 09:57:32 +0530

    fix: test commit from clone project

--- packages/DigiEx/src/types.ts
+++ packages/DigiEx/src/types.ts
@@ -4,3 +4,10 @@ export type DigiExTypes = {
   mediaType: "image" | "video";
   createdAt: string;
 };
+
+export type DigiExTypes2 = {
+  id: string;
+  name: string;
+  mediaType: "image" | "video";
+  createdAt: string;
+};
